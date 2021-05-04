0bb51f4975dd9213f0689268a5e43b482220ef06
dustinthegreat <dustinroberts96@gmail.com>
2021-05-04 16:30:55 -0400

    i could do REST in my sleep lol

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,1 +1,54 @@
  const Post = require("../models/postModel")
+
+
+
+ exports.getAllPost = async (req, res, next) => {
+ 	try {
+ 	const post = await Post.find()
+ 	res,status(200).json({
+ 		status:'yayyy', 
+ 		results:posts.length,	
+ 	});
+} catch(e) {
+ 		res.status(400).json({
+ 			status:'failed!!!!!!!!!!!', 
+
+ 		});
+
+
+ 	}
+ };
+
+
+ exports.getOnePost = async (req, res, next) => {
+ 	try {
+ 	const post = await Post.findById(req.params.id);
+ 	res,status(200).json({
+ 		status:'yayyy', 
+ 		results:posts.length,	
+ 	});
+} catch(e) {
+ 		res.status(400).json({
+ 			status:'failed!!!!!!!!!!!', 
+
+ 		});
+
+
+ 	}
+ exports.CreatePost = async (req, res, next) => {
+ 	try {
+ 	const post = await Post.create(req.body);
+ 	res,status(200).json({
+ 		status:'yayyy', 
+ 		results:posts.length,	
+ 	});
+} catch(e) {
+ 		res.status(400).json({
+ 			status:'failed!!!!!!!!!!!', 
+
+ 		});
+
+
+ 	}
+ };
+
