42291a606a25204c601fdaf1203d28dc6fb3b42e
Dustin <dustinroberts96@gmail.com>
2021-05-04 07:42:52 -0400

    adding config

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports =  {
+    MONGO_IP: process.env.MONGO_IP || "mongo",
+    MONGO_USER: process.env.USER || "root",
+    MONGO_PASSWORD: process.env.MONGO_PASSWORD || "toor"
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose  = require("mongoose");
-
+const {MONGO_USER, MONGO_PASSWORD, MONGO_IP}  = require('config/config');
 const app = express()
 
 
@@ -8,7 +8,6 @@ const app = express()
 mongoose.connect("mongodb://username:password@ip:port:path")
 .then(() => {console.log("Connected yo")
 });
-
 const port = process.env.PORT || 3003;
 
 
